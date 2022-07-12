a34b4b04f35c4c28148c98d5ef0951d20b6aa2f9
Grazac <grazactech@gmail.comn>
2022-07-12 11:19:32 +0100

    Initial Commit

---
+++ src/main/java/com/coinskash/CoinskashApplication.java
@@ -0,0 +1,13 @@
+package com.coinskash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinskashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinskashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coinskash/CoinskashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinskash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinskashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
